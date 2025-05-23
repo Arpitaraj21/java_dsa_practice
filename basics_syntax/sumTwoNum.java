73864050cf43b095d1ca2763bd964badec68a514
arpita0212 <arpita@pipra.solutions>
2025-05-23 17:11:30 +0530

    submmation of two numbers

---
+++ basics_syntax/sumTwoNum.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+// 2. Write a Program in Java to Add two Numbers.
+
+public class sumTwoNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = a + b;
+
+        System.out.println("Sum of two numbers: " + c);
+
+    }
+}
