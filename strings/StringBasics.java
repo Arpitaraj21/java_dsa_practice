1b6b94dd763a88af6b9bcda231b0d8796c53ec9c
arpita0212 <arpita@pipra.solutions>
2025-06-17 14:46:42 +0530

    string changes

---
+++ strings/StringBasics.java
@@ -0,0 +1,8 @@
+public class StringBasics{
+    public static void main(String[] args) {
+        String name = "Arpita";
+        System.out.println(name);
+        int arr[] = new int[3];
+        int brr[] = arr;
+    }
+}
