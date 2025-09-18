ec4069635adf072ea0f3d100fc7ff2766e9af7b3
arpita0212 <arpita@pipra.solutions>
2025-09-18 14:19:59 +0530

    [Q-3] added the insertion sort

---
+++ sorting_algo_practice_questions/insertion_sort.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+public class insertion_sort {
+
+    public static void main(String[] args) {
+        int[] arr = {64, 32, 25, 12, 22};
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+        System.out.println("sorted array: " + Arrays.toString(arr));
+    }
+}
