0838f261a718f79a0dde610bb950009d8fe6d908
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-09-27 16:44:48 +0530

    remove duplicate elements brute forec and optimized

---
+++ arrays/remove_duplicate_element/optimized.java
@@ -0,0 +1,14 @@
+public class optimized {
+    public static void main(String[] args){
+        int[] arr = {1,1,2,2,2,2,3,3};
+        int n = arr.length;
+        int i = arr[0];
+        int j = arr[1];
+        int count = 0;
+        for(int k=0; k<n;k++){
+            if (arr[i] != arr[j]){
+                
+            }
+        }
+    }
+}
