7bf3bba89e0575071ee156bedb00036b60454da9
arpita0212 <arpita@pipra.solutions>
2025-10-03 17:52:01 +0530

    Missing Number from an array brute force approach

---
+++ cses questions/MissingNumber.java
@@ -0,0 +1,27 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n - 1];
+
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            if (i + 1 != arr[i]) {
+                System.out.println(i + 1);
+                break;
+            }
+        }
+        sc.close();
+    }
+}
