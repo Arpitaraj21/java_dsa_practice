be1ddf0c76b2cccbae21dda1c63daf8069b9fcf0
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-10-06 08:26:49 +0530

    max consecutive ones

---
+++ arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+
+import static java.lang.Math.max;
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args){
+        int[] arr = {1,1,0,1,1,1,1,0,1,1};
+        int n = arr.length;
+        int count = 0;
+        int maxi = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == 1){
+                count++;
+                maxi = max(maxi, count);
+            }
+            else{
+                count = 0;
+            }
+        }
+        System.out.println(maxi);
+    }
+}
