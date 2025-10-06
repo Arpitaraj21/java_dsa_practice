fdb11147ecdbb451868d75c83b4f4063099e1e6a
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-10-06 08:55:03 +0530

    insertion sort question 4

---
+++ practice_set_revision/InsertionSortQuestion4.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class InsertionSortQuestion4 {
+    public static void main(String[] args) {
+        int[] arr = {1,4,5,2,3};
+        int n = arr.length;
+
+        for(int i=0; i<n;i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
