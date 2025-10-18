f8425e41ed0243c93e6f12065a3a0a7967c83187
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-10-18 18:32:45 +0530

    majority element optimized approach

---
+++ arrays/Majority Element/MajorityElementOptimizedApproach.java
@@ -0,0 +1,35 @@
+public class MajorityElementOptimizedApproach{
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 3, 3, 1, 2, 2};
+        int n = arr.length;
+
+        int count = 0;
+        int candidate = -1;
+
+        for(int i=0; i<n; i++){
+            if(count == 0){
+                candidate = arr[i];
+            }
+            else if(arr[i] == candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+
+        count = 0;
+        for(int i=0; i<n;i++){
+            if(arr[i] == candidate){
+                count++;
+            }
+        }
+
+        if(count > n/2){
+            System.out.println(candidate);
+        }
+        else{
+            System.out.println("no majority element found");
+        }
+    }
+}
