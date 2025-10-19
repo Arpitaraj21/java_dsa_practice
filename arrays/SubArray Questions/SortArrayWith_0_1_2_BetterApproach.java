5ea703f7f92da8d266808b9bc619270df437d4a3
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-10-19 09:50:49 +0530

    sorting the array with 0 1 and 2 using counting approach

---
+++ arrays/SubArray Questions/SortArrayWith_0_1_2_BetterApproach.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+
+public class SortArrayWith_0_1_2_BetterApproach {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,0,1,2,1,2,0,0,0,1};
+        int n = arr.length;
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i=0; i<n;i++){
+            if(arr[i] == 0){
+                count0++;
+            }
+            if(arr[i] == 1){
+                count1++;
+            }
+            if(arr[i] == 1){
+                count2++;
+            }
+        }
+
+        for(int i=0; i<count0; i++){
+            arr[i] = 0;
+        }
+        for(int i=count0; i<count0+count1; i++){
+            arr[i] = 1;
+        }
+        for(int i=count0+count1;i<n; i++){
+            arr[i] = 2;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
