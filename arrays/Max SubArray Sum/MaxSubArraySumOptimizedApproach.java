6dc86d2fb10e20db36481bd522549ccfa527f81f
arpita0212 <arpita@pipra.solutions>
2025-10-21 11:05:14 +0530

    max subarray sum using the kadane's algo

---
+++ arrays/Max SubArray Sum/MaxSubArraySumOptimizedApproach.java
@@ -0,0 +1,24 @@
+
+public class MaxSubArraySumOptimizedApproach {
+
+    public static void main(String[] args) {
+        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+        int n = arr.length;
+
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            maxSum = Math.max(maxSum, sum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        System.out.println(maxSum);
+
+    }
+}
